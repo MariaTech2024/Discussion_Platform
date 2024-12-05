60e8196eaf19118bbfd837ce2ba2268fdd928f1c
mariaMeNow <mariiamenow@gmail.com>
2024-12-05 13:18:42 +0200

    add vercel config

--- server/index.js
+++ server/index.js
@@ -69,7 +69,12 @@ io.on('connection', (socket) => {
   });
 });
 
+app.get("/", (req,res)=>{
+  res.send("Server is working!")
+});
+
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
